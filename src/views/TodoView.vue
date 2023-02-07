e684bf5f11970d1ee08eb05a312979a3bd41fb59
dvvinfo <dvvinfo@mail.ru>
2023-02-07 16:20:59 +0300

    added editing and deleting tasks

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -2,13 +2,31 @@
   <main>
     <h1>Create Todo</h1>
     <todo-creator @create-todo="createTodo" />
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <todo-item
+        v-for="(todo, index) in todoList"
+        :todo="todo"
+        :index="index"
+        :key="todo.id"
+        @toggle-complete="toggleTodoComplete"
+        @edit-todo="toggleEditTodo"
+        @update-todo="updateTodo"
+        @delete-todo="deleteTodo"
+      />
+    </ul>
+    <p v-else class="todos-msg">
+      <Icon icon="noto-v1:sad-but-relieved-face" />
+      <span>You have no todo's to complete! Add one!</span>
+    </p>
   </main>
 </template>
 
 <script setup>
 import { ref } from "@vue/reactivity";
 import { uid } from "uid";
+import { Icon } from "@iconify/vue";
 import TodoCreator from "../components/TodoCreator.vue";
+import TodoItem from "../components/TodoItem.vue";
 
 const todoList = ref([]);
 
@@ -19,6 +37,22 @@ const createTodo = (todo) => {
     isCompleted: null,
     isEditing: null,
   });
+};
+
+const toggleTodoComplete = (todoPos) => {
+  todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted;
+};
+
+const toggleEditTodo = (todoPos) => {
+  todoList.value[todoPos].isEditing = !todoList.value[todoPos].isEditing;
+};
+
+const updateTodo = (todoVal, todoPos) => {
+  todoList.value[todoPos].todo = todoVal;
+};
+
+const deleteTodo = (todoId) => {
+  todoList.value = todoList.value.filter((todo) => todo.id !== todoId);
 };
 </script>
 
@@ -33,6 +67,21 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+    padding: 0;
+  }
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
